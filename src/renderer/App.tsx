886b12b373c6bb14e2ab08c80410442fb2681721
Dyllon <dyllon.g@outlook.com>
2021-07-07 10:29:04 +0800

    Fix react imports

---
+++ src/renderer/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h4>Welcome to React, Electron and Typescript</h4>
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
